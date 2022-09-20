7a404fcb06567943b5430b27da66652fbcb45e9f
samolin <samolin.vlad@gmail.com>
2022-09-20 18:10:27 +0300

    set databases in setting.py

--- app/settings.py
+++ app/settings.py
@@ -78,8 +78,18 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 
 import dj_database_url
+def database():
+    if dj_database_url.config() == {}:
+        return {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
+    else:
+        return {'default': dj_database_url.config()}
 
-DATABASES = {'default': dj_database_url.config()}
+DATABASES =  database()
 
 
 
