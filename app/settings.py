103b62aa67be84e941d35e06468362998e623831
samolin <samolin.vlad@gmail.com>
2022-09-20 17:38:01 +0300

    set databases in settings.py

--- app/settings.py
+++ app/settings.py
@@ -76,12 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+import dj_database_url
+
+DATABASES = {'default': dj_database_url.config()}
+
 
 
 # Password validation
