f3db8af60439ba052492493894551cb9474a62d0
samolin <samolin.vlad@gmail.com>
2022-09-22 04:23:53 +0300

    set setting for another name of heroku

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['payment-stripe-test-test.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['stripe-payment-service.herokuapp.com','127.0.0.1']
 
 
 # Application definition
